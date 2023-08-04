7540ab640057ac1a8078aedbe82c688cf4750e6b
hehoku <hehoku@outlook.com>
2023-08-04 20:59:44 +0800

    chore: remove Supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import AuthForm from "./auth-form";
-
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center gap-10">
       <h1 className="text-3xl font-bold mt-10">6169</h1>
-      <AuthForm />
     </div>
   );
 }
