783d0fa1a4dcbf6112baf01f85e3f9b749f8048e
hehoku <hehoku@outlook.com>
2023-08-04 22:11:52 +0800

    feat: register page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { RegisterForm } from "./registerForm";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center gap-10">
       <h1 className="text-3xl font-bold mt-10">6169</h1>
+      <RegisterForm />
     </div>
   );
 }
